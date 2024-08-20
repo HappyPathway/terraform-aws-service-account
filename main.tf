6f45c41f81dbf8d42186fc3cb8df32b1fc7f090e
Dave Arnold <dave@roknsound.com>
2024-08-20 11:00:09 -0700

    chore: Update main.tf to use JSON format for admin_policy

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_iam_user_policy" "policy" {
   name   = aws_iam_user.user.name
   user   = aws_iam_user.user.name
-  policy = jsonencode(local.iam_policy)
+  policy = local.iam_policy
 }
 
 resource "aws_iam_access_key" "access_key" {
