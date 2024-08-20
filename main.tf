98d3706b335821b381a6392a75cd3476a54fd957
Dave Arnold <dave@roknsound.com>
2024-08-20 10:55:38 -0700

    chore: Update main.tf to use JSON format for admin_policy

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  iam_policy = var.iam_policy == "admin" ? data.aws_iam_policy_document.admin_policy : var.iam_policy
+  iam_policy = var.iam_policy == "admin" ? data.aws_iam_policy_document.admin_policy.json : var.iam_policy
 }
 
 resource "aws_iam_user_policy" "policy" {
