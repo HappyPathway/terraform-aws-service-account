a29a6d5b79de7b8b364d72c92386b74e40b52bee
Dave Arnold <dave@roknsound.com>
2024-08-20 10:19:08 -0700

    chore: Add AWS IAM user and access key resources

---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "iam_policy" {
+  type = string
+}
+
+variable "iam_user_name" {
+  type    = string
+  default = "s3-access-role"
+}
+
+variable "iam_policy_name" {
+  type    = string
+  default = "s3-access-policy"
+}
+
+resource "aws_iam_user" "user" {
+  name = var.iam_user_name
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name   = var.iam_policy_name
+  user   = aws_iam_user.user.name
+  policy = jsonencode(var.iam_policy)
+}
+
+resource "aws_iam_access_key" "access_key" {
+  user = aws_iam_user.user.name
+}
+
+output "iam_credentials" {
+  value = {
+    iam_user_name         = aws_iam_user.user.name
+    iam_access_key_id     = aws_iam_access_key.access_key.id
+    iam_secret_access_key = aws_iam_access_key.access_key.secret
+  }
+  description = "The IAM user credentials"
+  sensitive   = true
+}
